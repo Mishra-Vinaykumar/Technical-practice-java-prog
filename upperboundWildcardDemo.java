260b5bc2c34b82c9eaf5c9617f6addf587618df2
Mishra-Vinaykumar <vinaymishra9925@gmail.com>
2024-06-19 13:24:37 +0530

    lembdaFunction

--- upperboundWildcardDemo.java
+++ upperboundWildcardDemo.java
@@ -1,11 +1,19 @@
+import java.util.ArrayList;
+
 public class upperboundWildcardDemo {
 
-    public static Double sumOfList(ArrayList<? extends Number> al) {
-        double sum = 0.0;
-        for (Number n : al) {
-            sum = sum + n.doubleValue(); // any sub class
-        }
-        return sum;
-    }
+    public static void main(String[] args) {
+        ArrayList<Integer> intList = new ArrayList<Integer>();
+        intList.add(10);
+        intList.add(20);
+        // passing Integer List
+        System.out.println("sum = " + UpperBoundedWildCard.sumOfList(intList));
 
+        ArrayList<Double> doubleList = new ArrayList<Double>();
+        doubleList.add(10.0);
+        doubleList.add(20.0);
+        doubleList.add(30.0);
+        // passing Double List
+        System.out.println("sum = " + UpperBoundedWildCard.sumOfList(doubleList));
+    }
 }
